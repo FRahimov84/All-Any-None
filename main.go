440019ce0306e4c4b8f089869917c86148626946
Lambo <Lambo@localhost>
2020-01-25 16:22:28 +0500

    Done

--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
+type account struct {
+	id      int64
+	name    string
+	balance int64
+}
+type accountError struct {
+	message string
+}
 
+func (a accountError) Error() string {
+	return a.message
+}
+
+func all(items []account, predicate func(item account) bool) bool {
+	return index(items, predicate, true) == -1
+}
+
+func any(items []account, predicate func(item account) bool) bool {
+	return index(items, predicate, false) != -1
+}
+
+func none(items []account, predicate func(item account) bool) bool {
+	return index(items, predicate, false) == -1
+}
+
+func index(items []account, predicate func(item account) bool, reverse bool) int64 {
+	for _, value := range items {
+		if reverse {
+			if !predicate(value) {
+				return value.id
+			}
+		} else {
+			if predicate(value) {
+				return value.id
+			}
+		}
+	}
+	return -1
+}
+
+func find(items []account, predicate func(item account) bool) (account, error) {
+	result := index(items, predicate, false)
+	if result == -1 {
+		return account{}, accountError{message: "not found"}
+	}
+	return items[result-1], nil
+}
 
 func main() {}
---
+++ main_test.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+
+var data = []account{
+	{id: 1, name: "Vasya", balance: 1_000},
+	{id: 2, name: "Petya", balance: 2_000},
+	{id: 3, name: "Kostya", balance: 3_000},
+	{id: 4, name: "Oleg", balance: 10_000},
+	{id: 5, name: "Pavel", balance: 12_000},
+	{id: 6, name: "Katya", balance: 3_000},
+}
+
+func Example_AllForTrue() {
+	fmt.Println(all(data, func(item account) bool {
+		return item.balance >= 1_000
+	}))
+	// Output: true
+}
+func Example_AllForFalse() {
+	fmt.Println(all(data, func(item account) bool {
+		return item.balance >= 1_500
+	}))
+	// Output: false
+}
+
+func Example_AnyForTrue() {
+	fmt.Println(any(data, func(item account) bool {
+		return item.balance >= 0
+	}))
+	// Output: true
+}
+func Example_AnyForFalse() {
+	fmt.Println(any(data, func(item account) bool {
+		return item.balance > 12_000
+	}))
+	// Output: false
+}
+
+func Example_NoneForTrue() {
+	fmt.Println(none(data, func(item account) bool {
+		return item.balance == 3_001
+	}))
+	// Output: true
+}
+func Example_NoneForFalse() {
+	fmt.Println(none(data, func(item account) bool {
+		return item.balance != 3_000
+	}))
+	// Output: false
+}
+
+func Example_IndexWithResult() {
+	fmt.Println(index(data, func(item account) bool {
+		return item.balance > 3_000
+	},false))
+	// Output: 4
+}
+func Example_IndexWithoutResult() {
+	fmt.Println(index(data, func(item account) bool {
+		return item.balance <=300
+	},false))
+	// Output: -1
+}
+
+func Example_FindWithResult()  {
+	result, ok := find(data, func(item account) bool {
+		return item.balance == 3_000
+	})
+	if ok !=nil {
+		fmt.Errorf(ok.Error())
+	}else {
+		fmt.Println(result)
+	}
+	// Output: {3 Kostya 3000}
+}
+func Example_FindWithoutResult()  {
+	result, ok := find(data, func(item account) bool {
+		return item.name == "Oleg 1"
+	})
+	if ok !=nil {
+		fmt.Println(ok.Error())
+	}else {
+		fmt.Println(result)
+	}
+	// Output: not found
+}
